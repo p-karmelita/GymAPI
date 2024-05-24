fac0b7c0c57bef7521a4a1393ff35319267e2080
Piotr Karmelita <karpiotr90@gmail.com>
2024-05-24 18:31:46 +0200

    Create exercise model, add postgresql database.

--- app/sql/database.py
+++ app/sql/database.py
@@ -1,9 +1,7 @@
 import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
 
@@ -12,8 +10,9 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.environ.get('SQLALCHEMY_DATABASE_URL')
 
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- app/sql/models.py
+++ app/sql/models.py
@@ -1,12 +1,20 @@
 from sqlalchemy import Column, Integer, String
 
-
 from database import Base
 
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, index=True)
-    surname = Column(String, index=True)
-    hashed_password = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True, gt=1)
+    surname = Column(String, index=True, gt=1)
+    hashed_password = Column(String, lt=20)
+
+
+class Exercise(Base):
+    __tablename__ = 'exercise'
+    name = Column(String)
+    description = Column(String)
+    series = Column(Integer)
+    repetition = Column(Integer)
+    muscle_group = Column(String)
